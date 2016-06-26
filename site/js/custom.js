6d8594eb606a47ba875db6e25e7cb4e76a774efd
Zaher <zaher88abd@gmail.com>
2016-06-26 00:46:03 -0300

    Move fancybox script to new file Custom.js

---
+++ site/js/custom.js
@@ -0,0 +1,34 @@
+
+$(function() {
+$('a[href*="#"]:not([href="#"])').click(function() {
+  if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+    var target = $(this.hash);
+    target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+    if (target.length) {
+      $('html, body').animate({
+        scrollTop: target.offset().top
+      }, 1000);
+      return false;
+    }
+  }
+});
+});
+
+		$(document).ready(function() {
+			/*
+			 *  Simple image gallery. Uses default settings
+			 */
+       $(".various").fancybox({
+		maxWidth	: 600 ,
+//        maxHeight:100,
+		fitToView	: false,
+		width		: '100%',
+		height		: '100%',
+		autoSize	: false,
+		closeClick	: false,
+		openEffect	: 'none',
+		closeEffect	: 'none',
+           padding: [0,0,0,0],
+    closeBtn:true
+	});
+		});
